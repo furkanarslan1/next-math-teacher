14cd4ee6c76558e971022c43d7ab7c6dbaffad3c
furkanarslan1 <furkanarslandev@gmail.com>
2026-01-05 12:02:35 +0300

    feat:add test_action

---
+++ app/(Actions)/test/testAction.ts
@@ -0,0 +1,52 @@
+"use server";
+
+import { createServerSupabase } from "@/lib/supabase/server";
+import { revalidatePath } from "next/cache";
+
+export async function addQuestionAction(formData: FormData, testId: string) {
+  const supabase = await createServerSupabase();
+
+  const questionText = formData.get("question_text") as string;
+  const optionA = formData.get("option_a") as string;
+  const optionB = formData.get("option_b") as string;
+  const optionC = formData.get("option_c") as string;
+  const optionD = formData.get("option_d") as string;
+  const correctAnswer = formData.get("correct_answer") as string;
+  const imageFile = formData.get("question_image") as File;
+
+  let imageUrl = null;
+
+  // Eğer resim seçilmişse yükle
+  // Upload if image is selected
+  if (imageFile && imageFile.size > 0) {
+    const fileName = `${testId}/${Date.now()}-${imageFile.name}`;
+    const { data: uploadData, error: uploadError } = await supabase.storage
+      .from("test-questions")
+      .upload(fileName, imageFile);
+
+    if (uploadError) throw new Error("Image not upload");
+
+    const {
+      data: { publicUrl },
+    } = supabase.storage.from("text-questions").getPublicUrl(fileName);
+
+    imageUrl = publicUrl;
+  }
+
+  const { error } = await supabase.from("questions").insert([
+    {
+      test_id: testId,
+      question_text: questionText,
+      image_url: imageUrl,
+      option_a: optionA,
+      option_b: optionB,
+      option_c: optionC,
+      option_d: optionD,
+      correct_answer: correctAnswer,
+    },
+  ]);
+
+  if (error) throw new Error(error.message);
+
+  revalidatePath(`/admin/tests/${testId}`);
+}
