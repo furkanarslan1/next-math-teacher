21222c70deba02d75df8b50be0a0a4bdd9914a03
furkanarslan1 <furkanarslandev@gmail.com>
2026-01-09 16:26:51 +0300

    feat:add pricePlans_edit_action

--- app/(Actions)/pricePlan/pricePlansAction.ts
+++ app/(Actions)/pricePlan/pricePlansAction.ts
@@ -28,3 +28,23 @@ export async function createPricePlan(data: PricePlanFormValues) {
   revalidatePath("/admin/price-plans");
   return { success: true, data: result };
 }
+
+export async function updatePricePlan(id: string, data: PricePlanFormValues) {
+  const supabase = await createServerSupabase();
+
+  const { error } = await supabase
+    .from("price_plans")
+    .update({
+      title: data.title,
+      category: data.category,
+      price: data.price,
+      discount_rate: data.discount_rate,
+      features: data.features,
+      is_active: data.is_active,
+    })
+    .eq("id", id); //sadece bu id li satırı güncelle // Update only the row with this ID
+  if (error) return { success: false, error: error.message };
+
+  revalidatePath("/admin/pricePlans");
+  return { success: true };
+}
